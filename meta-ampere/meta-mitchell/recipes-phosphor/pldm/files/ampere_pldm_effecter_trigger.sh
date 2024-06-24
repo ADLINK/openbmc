tinhnampere/openbmc-dev

meta-ampere: mtmitchell: support pldm effecter trigger script
This commit enables BERTTrigger, NMITrigger, DRAMMThrotEn option in
ampere_pldm_effecter_trigger.sh scripts.

Tested:
  1. Run the scripts with supported options.
  2. The host behaviors should match with the action.

Signed-off-by: Thu Ba Nguyen <tbnguyen@amperecomputing.com>

meta-ampere/meta-mitchell/recipes-ampere/platform/ampere-utils/ampere_pldm_effecter_trigger.sh
@@ -1,0 +1,144 @@
+#!/bin/bash
+
+# Usage of this utility
+function usage() {
+    echo "Usage: ampere_pldm_effecter_trigger.sh [OPTION] <0/1> [type] [value]"
+    echo "SOCKET:"
+    echo "        0: Setting for S0"
+    echo "        1: Setting for S1"
+    echo "OPTION:"
+    echo "        -h|--help"
+    echo "        -g|--get <0/1> <type>   : To get value of one type"
+    echo "        -s|--set <0/1> <type> <value>   :To set value of one type"
+    echo "TYPE:"
+    echo "        PLimit: Set SoC power limit"
+    echo "        PMinPowerLimit: Set SoC power min limit"
+    echo "        DRAMMThrotEn: Enable DRAM Throt. Value 1"
+    echo "        BERTTrigger: Trigger one BERT event. Value 2"
+    echo "        NMITrigger: Trigger an non-maskable interrupt to the host processor. Value 1"
+}
+
+object="xyz.openbmc_project.PLDM"
+interfaceName="xyz.openbmc_project.Sensor.Value"
+propertyName="Value"
+
+listPathS0=(
+    "/xyz/openbmc_project/sensors/power/S0_PLimit"
+    "/xyz/openbmc_project/sensors/power/S0_PMin_Pwr_Lmt"
+    "/xyz/openbmc_project/sensors/power/S0_DRAM_MThrotEn"
+    "/xyz/openbmc_project/sensors/count/S0_Effecter_204"
+    "/xyz/openbmc_project/sensors/count/S0_Effecter_201"
+)
+
+listPathS1=(
+    "/xyz/openbmc_project/sensors/power/S1_PLimit"
+    "/xyz/openbmc_project/sensors/power/S1_PMin_Pwr_Lmt"
+    "/xyz/openbmc_project/sensors/power/S1_DRAM_MThrotEn"
+    "/xyz/openbmc_project/sensors/count/S1_Effecter_204"
+    "/xyz/openbmc_project/sensors/count/S1_Effecter_201"
+)
+
+listOptions=(
+    "PLimit"
+    "PMinPowerLimit"
+    "DRAMMThrotEn"
+    "NMITrigger"
+    "BERTTrigger"
+)
+
+function get_all_options() {
+    if [[ "$1" == "0" ]] || [[ "$1" == "" ]]; then
+        for idx in "${!listOptions[@]}"; do
+            str=$(busctl get-property "$object" "${listPathS0[$idx]}" "$interfaceName" "$propertyName")
+            val=$(echo "$str" | cut -d " " -f 2)
+            echo "[S0] ${listOptions[$idx]}: $val"
+        done
+    fi
+    if [[ "$1" == "1" ]] || [[ "$1" == "" ]]; then
+        for idx in "${!listOptions[@]}"; do
+            str=$(busctl get-property "$object" "${listPathS1[$idx]}" "$interfaceName" "$propertyName")
+            val=$(echo "$str" | cut -d " " -f 2)
+            echo "[S1] ${listOptions[$idx]}: $val"
+        done
+    fi
+}
+
+function get_option() {
+    if [ "$1" == "0" ]; then
+        str=$(busctl get-property "$object" "${listPathS0[$2]}" "$interfaceName" "$propertyName")
+        val=$(echo "$str" | cut -d " " -f 2)
+        echo "[S0] ${listOptions[$2]}: $val"
+    elif [ "$1" == "1" ]; then
+        str=$(busctl get-property "$object" "${listPathS1[$2]}" "$interfaceName" "$propertyName")
+        val=$(echo "$str" | cut -d " " -f 2)
+        echo "[S1] ${listOptions[$2]}: $val"
+    fi
+}
+
+function find_type_idx() {
+    for idx in "${!listOptions[@]}"; do
+        if [ "${listOptions[$idx]}" == "$1" ]; then
+            echo "$idx"
+            return 1
+        fi
+    done
+    echo "-1"
+}
+
+function set_option() {
+    if [ "$1" == "0" ]; then
+        str=$(busctl set-property "$object" "${listPathS0[$2]}" "$interfaceName" "$propertyName" d "$3")
+        echo "[S0]  Set ${listOptions[$2]}: to $3"
+    elif [ "$1" == "1" ]; then
+        str=$(busctl set-property "$object" "${listPathS1[$2]}" "$interfaceName" "$propertyName" d "$3")
+        echo "[S1] Set ${listOptions[$2]}: to $3"
+    fi
+}
+
+operation=$1
+socket=$2
+type=$3
+value=$4
+
+# Process operations
+case "$operation" in
+    "-g" | "--get")
+        if [[ "$socket" != "0" ]] && [[ "$socket" != "1" ]] && [[ "$socket" != "" ]]; then
+            echo "Invalid socket \"$socket\". Socket must be 0 or 1 or empty."
+            exit
+        fi
+
+        st=$(find_type_idx "$type")
+        if [ "$st" != "-1" ]; then
+            get_option "$socket" "$st"
+        else
+            get_all_options "$socket"
+        fi
+        ;;
+    "-s" | "--set")
+        if [[ "$socket" != "0" ]] && [[ "$socket" != "1" ]]; then
+            echo "Invalid socket \"$socket\". Socket must be 0 or 1 for set."
+            exit
+        fi
+
+        st=$(find_type_idx "$type")
+        if [ "$st" == "-1" ]; then
+            echo "Invalid option to set $type"
+            exit
+        fi
+
+        if [[ "$type" == "BERTTrigger" ]]; then
+            value="2"
+            set_option "$socket" "$st" "$value"
+        elif [[ "$type" == "NMITrigger" ]] || [[ "$type" == "DRAMMThrotEn" ]]; then
+            value="1"
+            set_option "$socket" "$st" "$value"
+        else
+            set_option "$socket" "$st" "$value"
+        fi
+        ;;
+    "-h" | "--help" | "")
+            usage;
+            ;;
+esac
+

ADLINK/openbmc

meta-ampere/meta-mitchell/recipes-phosphor/pldm/files/ampere_pldm_effecter_trigger.sh
@@ -1,0 +1,144 @@
+#!/bin/bash
+
+# Usage of this utility
+function usage() {
+    echo "Usage: ampere_pldm_effecter_trigger.sh [OPTION] <0/1> [type] [value]"
+    echo "SOCKET:"
+    echo "        0: Setting for S0"
+    echo "        1: Setting for S1"
+    echo "OPTION:"
+    echo "        -h|--help"
+    echo "        -g|--get <0/1> <type>   : To get value of one type"
+    echo "        -s|--set <0/1> <type> <value>   :To set value of one type"
+    echo "TYPE:"
+    echo "        PLimit: Set SoC power limit"
+    echo "        PMinPowerLimit: Set SoC power min limit"
+    echo "        DRAMMThrotEn: Enable DRAM Throt. Value 1"
+    echo "        BERTTrigger: Trigger one BERT event. Value 2"
+    echo "        NMITrigger: Trigger an non-maskable interrupt to the host processor. Value 1"
+}
+
+object="xyz.openbmc_project.PLDM"
+interfaceName="xyz.openbmc_project.Sensor.Value"
+propertyName="Value"
+
+listPathS0=(
+    "/xyz/openbmc_project/sensors/power/S0_PLimit"
+    "/xyz/openbmc_project/sensors/power/S0_PMin_Pwr_Lmt"
+    "/xyz/openbmc_project/sensors/oem/S0_DRAM_MThrotEn"
+    "/xyz/openbmc_project/sensors/count/S0_Effecter_204"
+    "/xyz/openbmc_project/sensors/count/S0_Effecter_201"
+)
+
+listPathS1=(
+    "/xyz/openbmc_project/sensors/power/S1_PLimit"
+    "/xyz/openbmc_project/sensors/power/S1_PMin_Pwr_Lmt"
+    "/xyz/openbmc_project/sensors/oem/S1_DRAM_MThrotEn"
+    "/xyz/openbmc_project/sensors/count/S1_Effecter_204"
+    "/xyz/openbmc_project/sensors/count/S1_Effecter_201"
+)
+
+listOptions=(
+    "PLimit"
+    "PMinPowerLimit"
+    "DRAMMThrotEn"
+    "NMITrigger"
+    "BERTTrigger"
+)
+
+function get_all_options() {
+    if [[ "$1" == "0" ]] || [[ "$1" == "" ]]; then
+        for idx in "${!listOptions[@]}"; do
+            str=$(busctl get-property "$object" "${listPathS0[$idx]}" "$interfaceName" "$propertyName")
+            val=$(echo "$str" | cut -d " " -f 2)
+            echo "[S0] ${listOptions[$idx]}: $val"
+        done
+    fi
+    if [[ "$1" == "1" ]] || [[ "$1" == "" ]]; then
+        for idx in "${!listOptions[@]}"; do
+            str=$(busctl get-property "$object" "${listPathS1[$idx]}" "$interfaceName" "$propertyName")
+            val=$(echo "$str" | cut -d " " -f 2)
+            echo "[S1] ${listOptions[$idx]}: $val"
+        done
+    fi
+}
+
+function get_option() {
+    if [ "$1" == "0" ]; then
+        str=$(busctl get-property "$object" "${listPathS0[$2]}" "$interfaceName" "$propertyName")
+        val=$(echo "$str" | cut -d " " -f 2)
+        echo "[S0] ${listOptions[$2]}: $val"
+    elif [ "$1" == "1" ]; then
+        str=$(busctl get-property "$object" "${listPathS1[$2]}" "$interfaceName" "$propertyName")
+        val=$(echo "$str" | cut -d " " -f 2)
+        echo "[S1] ${listOptions[$2]}: $val"
+    fi
+}
+
+function find_type_idx() {
+    for idx in "${!listOptions[@]}"; do
+        if [ "${listOptions[$idx]}" == "$1" ]; then
+            echo "$idx"
+            return 1
+        fi
+    done
+    echo "-1"
+}
+
+function set_option() {
+    if [ "$1" == "0" ]; then
+        str=$(busctl set-property "$object" "${listPathS0[$2]}" "$interfaceName" "$propertyName" d "$3")
+        echo "[S0]  Set ${listOptions[$2]}: to $3"
+    elif [ "$1" == "1" ]; then
+        str=$(busctl set-property "$object" "${listPathS1[$2]}" "$interfaceName" "$propertyName" d "$3")
+        echo "[S1] Set ${listOptions[$2]}: to $3"
+    fi
+}
+
+operation=$1
+socket=$2
+type=$3
+value=$4
+
+# Process operations
+case "$operation" in
+    "-g" | "--get")
+        if [[ "$socket" != "0" ]] && [[ "$socket" != "1" ]] && [[ "$socket" != "" ]]; then
+            echo "Invalid socket \"$socket\". Socket must be 0 or 1 or empty."
+            exit
+        fi
+
+        st=$(find_type_idx "$type")
+        if [ "$st" != "-1" ]; then
+            get_option "$socket" "$st"
+        else
+            get_all_options "$socket"
+        fi
+        ;;
+    "-s" | "--set")
+        if [[ "$socket" != "0" ]] && [[ "$socket" != "1" ]]; then
+            echo "Invalid socket \"$socket\". Socket must be 0 or 1 for set."
+            exit
+        fi
+
+        st=$(find_type_idx "$type")
+        if [ "$st" == "-1" ]; then
+            echo "Invalid option to set $type"
+            exit
+        fi
+
+        if [[ "$type" == "BERTTrigger" ]]; then
+            value="2"
+            set_option "$socket" "$st" "$value"
+        elif [[ "$type" == "NMITrigger" ]] || [[ "$type" == "DRAMMThrotEn" ]]; then
+            value="1"
+            set_option "$socket" "$st" "$value"
+        else
+            set_option "$socket" "$st" "$value"
+        fi
+        ;;
+    "-h" | "--help" | "")
+            usage;
+            ;;
+esac
+